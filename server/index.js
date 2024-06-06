8777a62397c502ba5809b79b404785e0ebf0380c
aashiyatanwar <tanwaraashiya12@gmail.com>
2024-06-06 14:07:56 +0530

    Deployment Changes

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const { default: mongoose } = require("mongoose");
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
